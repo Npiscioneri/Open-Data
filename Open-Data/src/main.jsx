d4d0aac419b57fc8d300f44d65fa67f626f8c2b8
Niccolo' Piscioneri <piscioneri.niccolo@issvigano.org>
2025-03-22 11:02:39 +0100

    creazione delle carte

--- Open-Data/src/main.jsx
+++ Open-Data/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Card from './components/Card.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Card title="Card Title" content="This is a brief content for the card." />
+    <Card title="Second Card" content="This is the content for the second card." />
+    <Card title="Third Card" content="This is the content for the third card." />
   </StrictMode>,
 )
